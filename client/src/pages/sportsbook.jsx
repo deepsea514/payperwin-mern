14d9a2aa02436e970da17e146f5da8974da21c45
Snow White <74522790+snowwhite-boss@users.noreply.github.com>
2021-04-02 11:10:03 +0900

    show popup in sportsbook

--- client/src/pages/sportsbook.jsx
+++ client/src/pages/sportsbook.jsx
@@ -11,6 +11,7 @@ class SportsBook extends PureComponent {
         this.state = {
             loginUrl: null,
             loading: false,
+            showModal: true,
         };
     }
 
@@ -32,20 +33,48 @@ class SportsBook extends PureComponent {
 
 
     render() {
-        const { loginUrl, loading } = this.state;
-        if (loading) {
-            return <div>Loading...</div>;
-        }
-        if (loginUrl == null)
-            return <div>Error getting sportsbook.</div>;
+        const { loginUrl, loading, showModal } = this.state;
 
         return (
             <div className="row">
-                <Iframe url={loginUrl}
-                    width="100%"
-                    height="700px"
-                    display="initial"
-                    position="relative" />
+                {showModal && <div className="modal confirmation">
+                    <div className="background-closer" onClick={() => this.setState({ showModal: false })} />
+                    <div className="col-in">
+                        <i className="fal fa-times" style={{ cursor: 'pointer' }} onClick={() => this.setState({ showModal: false })} />
+                        <div>
+                            <br />
+                            <b>Instant Bet/ Live bet</b>
+                            <hr />
+                            <p>
+                                Welcome, here is where you can bet instantly.
+                                Your bets do not need to wait for a peer to accept.
+                                These bets are instantly accepted.
+                                The only catch is that the odds are different than PEER 2 PEER.
+                                The bets here are forwarded on to a sportsbook.
+                                We are in no way affiliated with the sportsbooks.
+                                Payper win guarantees you will be paid on all winning bets.
+                                We have found the best sportsbook odds for you.
+                                Payper win does not profit off of or charge any fees whatsoever on live or instant bets.
+                                All live betting or non matching bets are done here such soccer draw bets and parlays.
+                            </p>
+                            <div className="text-right">
+                                <button className="form-button" onClick={() => this.setState({ showModal: false })}> Don't show again </button>
+                                <button className="form-button ml-2" onClick={() => this.setState({ showModal: false })}> OK </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>}
+                {(() => {
+                    if (loading)
+                        return <div>Loading...</div>;
+                    if (loginUrl == null)
+                        return <div>Error getting sportsbook.</div>;
+                    return <Iframe url={loginUrl}
+                        width="100%"
+                        height="700px"
+                        display="initial"
+                        position="relative" />
+                })()}
             </div>
         );
     }
