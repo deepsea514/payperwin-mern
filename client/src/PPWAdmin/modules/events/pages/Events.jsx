bb3d825c6e6d8860809bba0f10b815e9e24b8f1f
Black Storm <nightyellowstormy@gmail.com>
2021-06-03 17:41:30 +0900

    add events filter

--- client/src/PPWAdmin/modules/events/pages/Events.jsx
+++ client/src/PPWAdmin/modules/events/pages/Events.jsx
@@ -64,7 +64,7 @@ class Events extends React.Component {
 
     getBetOptions = (candidates) => {
         return candidates.map(candidate => (
-            <p>{candidate.name}: {candidate.odds[candidate.odds.length - 1]}</p>
+            <p key={candidate.name}>{candidate.name}: {candidate.odds[candidate.odds.length - 1]}</p>
         ));
     }
 
