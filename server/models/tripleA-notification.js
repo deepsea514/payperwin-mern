38853e2825ff1db03cd9b690310f9182dd2512ba
Snow White <neoprince1992@gmail.com>
2021-06-25 11:30:12 +0900

    add timestamps in models

--- server/models/tripleA-notification.js
+++ server/models/tripleA-notification.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const TripleANotificationSchema = new Schema({
-    body: Object,
-    rawBody: String,
-    signature: String,
-    succeed: { type: Boolean, default: false }
-});
+const TripleANotificationSchema = new Schema(
+    {
+        body: Object,
+        rawBody: String,
+        signature: String,
+        succeed: { type: Boolean, default: false }
+    },
+    {
+        timestamps: true,
+    }
+);
 
 const TripleANotification = mongoose.model('TripleANotification', TripleANotificationSchema);
 module.exports = TripleANotification;
--- server/models/v1requests.js
+++ server/models/v1requests.js
@@ -2,11 +2,16 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const V1RequestSchema = new Schema({
-    request: Object,
-    type: String,
-    params: Object
-});
+const V1RequestSchema = new Schema(
+    {
+        request: Object,
+        type: String,
+        params: Object
+    },
+    {
+        timestamps: true,
+    }
+);
 
 const V1Request = mongoose.model('V1Request', V1RequestSchema);
 
