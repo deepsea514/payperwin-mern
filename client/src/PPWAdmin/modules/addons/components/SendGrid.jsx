dd5363e38c5a0197c4bfffe70dd1e175b50e4d6f
Black Storm <nightyellowstormy@gmail.com>
2021-07-02 09:35:45 +0900

    make sendgrid key

---
+++ client/src/PPWAdmin/modules/addons/components/SendGrid.jsx
@@ -0,0 +1,169 @@
+import React from "react";
+import * as Yup from "yup";
+import { Formik } from "formik";
+import { Preloader, ThreeDots } from 'react-preloader-icon';
+import { getAddon, setAddon } from "../redux/services";
+import SVG from "react-inlinesvg";
+
+export default class SendGrid extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: false,
+            initialValues: null,
+            sendgridSchema: Yup.object().shape({
+                sendgridApiKey: Yup.string()
+                    .required("SendGrid APi Key is required"),
+            }),
+            isError: false,
+            isSuccess: false,
+        }
+    }
+
+    componentDidMount() {
+        this.setState({ loading: false });
+        getAddon('pinnacle')
+            .then(({ data }) => {
+                if (data) {
+                    this.setState({ initialValues: data.value, loading: false });
+                } else {
+                    this.setState({
+                        initialValues: {
+                            sendgridApiKey: "",
+                        }, loading: false
+                    });
+                }
+            })
+            .catch(() => {
+                this.setState({ initialValues: null, loading: false });
+            })
+    }
+
+    onSubmit = (values, formik) => {
+        this.setState({ isError: false, isError: false, });
+        setAddon('pinnacle', values)
+            .then(() => {
+                this.setState({ isSuccess: true })
+                formik.setSubmitting(false);
+            })
+            .catch(() => {
+                this.setState({ isError: true })
+                formik.setSubmitting(false);
+            })
+    }
+
+    getInputClasses = (formik, fieldname) => {
+        if (formik.touched[fieldname] && formik.errors[fieldname]) {
+            return "is-invalid";
+        }
+        if (formik.touched[fieldname] && !formik.errors[fieldname]) {
+            return "is-valid";
+        }
+        return "";
+    };
+
+    render() {
+        const { loading, initialValues, sendgridSchema, isError, isSuccess } = this.state;
+        return (
+            <div className="mt-3">
+                <div className="d-flex justify-content-between">
+                    <h3>SendGrid</h3>
+                    <img src="/images/sendgrid.png" style={{ display: 'block', height: '40px', width: 'auto' }} />
+                </div>
+                {loading && <center className="mt-5"><Preloader use={ThreeDots}
+                    size={100}
+                    strokeWidth={10}
+                    strokeColor="#F0AD4E"
+                    duration={800} /></center>}
+                {!loading && initialValues == null && <h1>No data available</h1>}
+                {!loading && initialValues && <Formik
+                    validationSchema={sendgridSchema}
+                    initialValues={initialValues}
+                    onSubmit={this.onSubmit}
+                >
+                    {(formik) => {
+                        return <form onSubmit={formik.handleSubmit} className="p-3">
+                            {isError && (
+                                <div
+                                    className="alert alert-custom alert-light-danger fade show mb-10"
+                                    role="alert"
+                                >
+                                    <div className="alert-icon">
+                                        <span className="svg-icon svg-icon-3x svg-icon-danger">
+                                            <SVG
+                                                src={"/media/svg/icons/Code/Info-circle.svg"}
+                                            ></SVG>{" "}
+                                        </span>
+                                    </div>
+                                    <div className="alert-text font-weight-bold">
+                                        Update Failed
+                                    </div>
+                                    <div className="alert-close" onClick={() => this.setState({ isError: false })}>
+                                        <button
+                                            type="button"
+                                            className="close"
+                                            data-dismiss="alert"
+                                            aria-label="Close"
+                                        >
+                                            <span aria-hidden="true">
+                                                <i className="ki ki-close"></i>
+                                            </span>
+                                        </button>
+                                    </div>
+                                </div>
+                            )}
+                            {isSuccess && (
+                                <div
+                                    className="alert alert-custom alert-light-success fade show mb-10"
+                                    role="alert"
+                                >
+                                    <div className="alert-icon">
+                                        <span className="svg-icon svg-icon-3x svg-icon-success">
+                                            <SVG
+                                                src={"/media/svg/icons/Code/Info-circle.svg"}
+                                            ></SVG>{" "}
+                                        </span>
+                                    </div>
+                                    <div className="alert-text font-weight-bold">
+                                        Successfully Updated.
+                                    </div>
+                                    <div className="alert-close" onClick={() => this.setState({ isSuccess: false })}>
+                                        <button
+                                            type="button"
+                                            className="close"
+                                            data-dismiss="alert"
+                                            aria-label="Close"
+                                        >
+                                            <span aria-hidden="true">
+                                                <i className="ki ki-close"></i>
+                                            </span>
+                                        </button>
+                                    </div>
+                                </div>
+                            )}
+
+                            <div className="form-group">
+                                <label>SendGrid Api Key<span className="text-danger">*</span></label>
+                                <input type="text" name="sendgridApiKey" className={`form-control ${this.getInputClasses(
+                                    formik,
+                                    "sendgridApiKey"
+                                )}`}
+                                    {...formik.getFieldProps("sendgridApiKey")}
+                                    placeholder="SendGrid Api Key" />
+                                {formik.touched.sendgridApiKey && formik.errors.sendgridApiKey ? (
+                                    <div className="invalid-feedback">
+                                        {formik.errors.sendgridApiKey}
+                                    </div>
+                                ) : null}
+                            </div>
+
+                            <div className="form-row">
+                                <button type="submit" className="btn btn-primary mr-2" disabled={formik.isSubmitting}>Submit</button>
+                            </div>
+                        </form>
+                    }}
+                </Formik>}
+            </div>
+        )
+    }
+}
