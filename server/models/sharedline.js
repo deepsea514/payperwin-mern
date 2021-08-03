b033eb8be4cac21c3e85dd0f16606b9345991d7f
Snow White <neoprince1992@gmail.com>
2021-08-03 10:19:25 +0900

    save shared lines

---
+++ server/models/sharedline.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const SharedLineSchema = new Schema(
+    {
+        user: { type: Schema.Types.ObjectId, ref: "User" },
+        url: { type: String, required: true },
+        eventDate: { type: Date, required: true },
+        uniqueId: { type: String, required: true },
+        type: { type: String, required: true },
+        index: { type: Number },
+    },
+    {
+        timestamps: true,
+    },
+);
+
+const SharedLine = mongoose.model('SharedLine', SharedLineSchema);
+
+module.exports = SharedLine;
