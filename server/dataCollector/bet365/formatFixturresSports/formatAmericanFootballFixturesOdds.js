d924504c189bd7a09fd24c0881271a3277da487e
Snow White <neoprince1992@gmail.com>
2021-08-24 16:02:50 +0900

    fix rugby and american football

--- server/dataCollector/bet365/formatFixturresSports/formatAmericanFootballFixturesOdds.js
+++ server/dataCollector/bet365/formatFixturresSports/formatAmericanFootballFixturesOdds.js
@@ -14,39 +14,31 @@ function formatAmericanFootballFixturesOdds(event) {
     }
 
     if (main && main.sp.game_lines) {
-        const game_lines = main.sp.game_lines;
-        const count = game_lines.length / 3;
+        const game_lines = main.sp.game_lines.odds;
+        const count = game_lines.length / 2;
         for (let i = 0; i < count; i++) {
             if (game_lines[i].name == 'Spread') {
                 line.spreads.push({
-                    altLineId: game_lines[i + count].id,
-                    hdp: Number(game_lines[i + count].handicap),
-                    home: convertDecimalToAmericanOdds(game_lines[i + count].odds),
-                    away: convertDecimalToAmericanOdds(game_lines[i + count * 2].odds),
+                    altLineId: game_lines[i].id,
+                    hdp: Number(game_lines[i].handicap),
+                    home: convertDecimalToAmericanOdds(game_lines[i].odds),
+                    away: convertDecimalToAmericanOdds(game_lines[i + count].odds),
                 });
             }
             if (game_lines[i].name == 'Total') {
                 line.totals.push({
-                    altLineId: game_lines[i + count].id,
-                    points: Number(game_lines[i + count].handicap),
-                    over: convertDecimalToAmericanOdds(game_lines[i + count].odds),
-                    under: convertDecimalToAmericanOdds(game_lines[i + count * 2].odds),
+                    altLineId: game_lines[i].id,
+                    points: Number(game_lines[i].handicap),
+                    over: convertDecimalToAmericanOdds(game_lines[i].odds),
+                    under: convertDecimalToAmericanOdds(game_lines[i + count].odds),
                 })
             }
             if (game_lines[i].name == 'Money Line') {
                 line.moneyline = {
-                    home: convertDecimalToAmericanOdds(game_lines[i + count].odds),
-                    away: convertDecimalToAmericanOdds(game_lines[i + count * 2].odds),
+                    home: convertDecimalToAmericanOdds(game_lines[i].odds),
+                    away: convertDecimalToAmericanOdds(game_lines[i + count].odds),
                 }
             }
-        }
-    }
-
-    if (!line.moneyline && schedule && schedule.sp.main) {
-        const moneyline = schedule.sp.main;
-        line.moneyline = {
-            home: convertDecimalToAmericanOdds(moneyline[0].odds),
-            away: convertDecimalToAmericanOdds(moneyline[1].odds)
         }
     }
 
