25731ef925383f4c8a656e1ad46351dd890f16fb
Black Storm <nightyellowstormy@gmail.com>
2021-08-24 15:47:00 +0900

    got cricket odds

--- server/dataCollector/bet365/formatFixturresSports/formatCricketFixturesOdds.js
+++ server/dataCollector/bet365/formatFixturresSports/formatCricketFixturesOdds.js
@@ -1,6 +1,6 @@
 const { convertDecimalToAmericanOdds } = require('../convertOdds');
 function formatCricketFixturesOdds(event) {
-    const { schedule } = event.odds;
+    const { main, schedule } = event.odds;
     let line = {
         originId: event.id,
         endDate: new Date(parseInt(event.time) * 1000),
@@ -10,7 +10,15 @@ function formatCricketFixturesOdds(event) {
         totals: [],
     }
 
-    if (schedule && schedule.sp.main) {
+    if (main && main.sp.to_win_the_match) {
+        const moneyline = main.sp.to_win_the_match.odds;
+        line.moneyline = {
+            home: convertDecimalToAmericanOdds(moneyline[0].odds),
+            away: convertDecimalToAmericanOdds(moneyline[1].odds)
+        }
+    }
+
+    if (!line.moneyline && schedule && schedule.sp.main) {
         const moneyline = schedule.sp.main;
         line.moneyline = {
             home: convertDecimalToAmericanOdds(moneyline[0].odds),
