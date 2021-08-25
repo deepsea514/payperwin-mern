a2906fa88cf2a275a2c12b2c77f81af1f3aefe59
Black Storm <nightyellowstormy@gmail.com>
2021-08-25 09:52:01 +0900

    fix volleyball

--- server/dataCollector/bet365/formatFixturresSports/formatFloorballFixturesOdds.js
+++ server/dataCollector/bet365/formatFixturresSports/formatFloorballFixturesOdds.js
@@ -1,7 +1,8 @@
 const { convertDecimalToAmericanOdds } = require('../convertOdds');
 const TestEvent = require('../../../models/testEvent');
 function formatFloorballFixturesOdds(event) {
-    const { main, schedule } = event.odds;
+    TestEvent.create({ event, name: 'floorball' });
+    const { main } = event.odds;
     let line = {
         originId: event.id,
         endDate: new Date(parseInt(event.time) * 1000),
@@ -11,30 +12,30 @@ function formatFloorballFixturesOdds(event) {
         totals: [],
     }
 
-    if (main && main.sp['3_way_betting']) {
-        const _3_way_betting = main.sp['3_way_betting'];
-        const line_count = _3_way_betting.length / 4;
-        for (let i = 0; i < line_count; i++) {
-            if (_3_way_betting[i].name == "Money Line") {
-                line.moneyline = {
-                    home: convertDecimalToAmericanOdds(_3_way_betting[i + line_count].odds),
-                    away: convertDecimalToAmericanOdds(_3_way_betting[i + line_count * 3].odds)
-                }
-            } else if (_3_way_betting[i].name == "Handicap") {
-                line.spreads.push({
-                    hdp: Number(_3_way_betting[i + line_count].handicap),
-                    home: convertDecimalToAmericanOdds(_3_way_betting[i + line_count].odds),
-                    away: convertDecimalToAmericanOdds(_3_way_betting[i + line_count * 3].odds),
-                })
-            } else if (_3_way_betting[i].name == "Total") {
-                line.totals.push({
-                    points: Number(_3_way_betting[i + line_count].handicap),
-                    over: convertDecimalToAmericanOdds(_3_way_betting[i + line_count].odds),
-                    under: convertDecimalToAmericanOdds(_3_way_betting[i + line_count * 3].odds),
-                })
-            }
-        }
-    }
+    // if (main && main.sp['3_way_betting']) {
+    //     const _3_way_betting = main.sp['3_way_betting'];
+    //     const line_count = _3_way_betting.length / 4;
+    //     for (let i = 0; i < line_count; i++) {
+    //         if (_3_way_betting[i].name == "Money Line") {
+    //             line.moneyline = {
+    //                 home: convertDecimalToAmericanOdds(_3_way_betting[i + line_count].odds),
+    //                 away: convertDecimalToAmericanOdds(_3_way_betting[i + line_count * 3].odds)
+    //             }
+    //         } else if (_3_way_betting[i].name == "Handicap") {
+    //             line.spreads.push({
+    //                 hdp: Number(_3_way_betting[i + line_count].handicap),
+    //                 home: convertDecimalToAmericanOdds(_3_way_betting[i + line_count].odds),
+    //                 away: convertDecimalToAmericanOdds(_3_way_betting[i + line_count * 3].odds),
+    //             })
+    //         } else if (_3_way_betting[i].name == "Total") {
+    //             line.totals.push({
+    //                 points: Number(_3_way_betting[i + line_count].handicap),
+    //                 over: convertDecimalToAmericanOdds(_3_way_betting[i + line_count].odds),
+    //                 under: convertDecimalToAmericanOdds(_3_way_betting[i + line_count * 3].odds),
+    //             })
+    //         }
+    //     }
+    // }
 
     if (line.moneyline && (!line.moneyline.home || !line.moneyline.away)) {
         line.moneyline = null
