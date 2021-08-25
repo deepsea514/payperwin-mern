f91f1c1b16ebf1d9abd262af25518e79361a3bdf
Black Storm <nightyellowstormy@gmail.com>
2021-08-25 09:12:53 +0900

    fix baseball odds

--- server/dataCollector/bet365/formatFixturresSports/formatBaseballFixturesOdds.js
+++ server/dataCollector/bet365/formatFixturresSports/formatBaseballFixturesOdds.js
@@ -10,39 +10,39 @@ function formatBaseballFixturesOdds(event) {
         totals: [],
     }
 
-    if (schedule && schedule.sp.main) {
-        const moneyline = schedule.sp.main;
-        line.moneyline = {
-            home: convertDecimalToAmericanOdds(moneyline[0].odds),
-            away: convertDecimalToAmericanOdds(moneyline[1].odds)
-        }
-    }
+    // if (schedule && schedule.sp.main) {
+    //     const moneyline = schedule.sp.main;
+    //     line.moneyline = {
+    //         home: convertDecimalToAmericanOdds(moneyline[0].odds),
+    //         away: convertDecimalToAmericanOdds(moneyline[1].odds)
+    //     }
+    // }
 
-    if (main && Object.keys(main.sp).length > 0) {
-        const game_lines = main.sp.game_lines;
-        if (game_lines) {
-            const count = game_lines.length / 3;
+    if (main) {
+        if (main.sp.game_lines) {
+            const game_lines = main.sp.game_lines.odds;
+            const count = game_lines.length / 2;
             for (let i = 0; i < count; i++) {
                 if (game_lines[i].name == 'Run Line') {
                     line.spreads.push({
-                        altLineId: game_lines[i + count].id,
-                        hdp: Number(game_lines[i + count].handicap),
-                        home: convertDecimalToAmericanOdds(game_lines[i + count].odds),
-                        away: convertDecimalToAmericanOdds(game_lines[i + count * 2].odds),
+                        altLineId: game_lines[i].id,
+                        hdp: Number(game_lines[i].handicap),
+                        home: convertDecimalToAmericanOdds(game_lines[i].odds),
+                        away: convertDecimalToAmericanOdds(game_lines[i + count].odds),
                     });
                 }
                 if (game_lines[i].name == 'Total') {
                     line.totals.push({
-                        altLineId: game_lines[i + count].id,
-                        points: Number(game_lines[i + count].handicap),
-                        over: convertDecimalToAmericanOdds(game_lines[i + count].odds),
-                        under: convertDecimalToAmericanOdds(game_lines[i + count * 2].odds),
+                        altLineId: game_lines[i].id,
+                        points: Number(game_lines[i].handicap.slice(2, game_lines[i].handicap.length)),
+                        over: convertDecimalToAmericanOdds(game_lines[i].odds),
+                        under: convertDecimalToAmericanOdds(game_lines[i + count].odds),
                     })
                 }
                 if (game_lines[i].name == 'Money Line' && !line.moneyline) {
                     line.moneyline = {
-                        home: convertDecimalToAmericanOdds(game_lines[i + count].odds),
-                        away: convertDecimalToAmericanOdds(game_lines[i + count * 2].odds)
+                        home: convertDecimalToAmericanOdds(game_lines[i].odds),
+                        away: convertDecimalToAmericanOdds(game_lines[i + count].odds)
                     }
                 }
             }
