0604643ad43dc048482cf2ac91770c1d43737005
Black Storm <nightyellowstormy@gmail.com>
2021-08-26 10:18:12 +0900

    fix errors in getting odds

--- server/dataCollector/bet365/formatFixturresSports/formatBadmintonFixturesOdds.js
+++ server/dataCollector/bet365/formatFixturresSports/formatBadmintonFixturesOdds.js
@@ -11,9 +11,33 @@ function formatBadmintonFixturesOdds(event) {
         totals: [],
     }
 
-    if (main) {
-        if (main.sp.match_lines);
+    if (main && main.sp.match_lines) {
         const match_lines = main.sp.match_lines.odds;
+        const line_count = match_lines.length / 2;
+        for (let i = 0; i < line_count; i++) {
+            if (match_lines[i].name == "To Win") {
+                line.moneyline = {
+                    home: convertDecimalToAmericanOdds(match_lines[i].odds),
+                    away: convertDecimalToAmericanOdds(match_lines[i + line_count].odds)
+                }
+            } else if (match_lines[i].name == "Spread") {
+                line.spreads.push({
+                    altLineId: match_lines[i].id,
+                    hdp: Number(match_lines[i].handicap),
+                    home: convertDecimalToAmericanOdds(match_lines[i].odds),
+                    away: convertDecimalToAmericanOdds(match_lines[i + line_count].odds),
+                })
+            } else if (match_lines[i].name == "Total") {
+                line.totals.push({
+                    altLineId: match_lines[i].id,
+                    points: Number(match_lines[i].handicap.slice(2, match_lines[i].handicap.length)),
+                    over: convertDecimalToAmericanOdds(match_lines[i].odds),
+                    under: convertDecimalToAmericanOdds(match_lines[i + line_count].odds),
+                })
+            }
+        }
+    } else if (match && match.sp.match_lines) {
+        const match_lines = match.sp.match_lines.odds;
         const line_count = match_lines.length / 2;
         for (let i = 0; i < line_count; i++) {
             if (match_lines[i].name == "To Win") {
