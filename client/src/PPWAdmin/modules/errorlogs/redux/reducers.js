7d711cfd5f7dc2768a3a11740ac7a9f0e5847e0f
Snow White <neoprince1992@gmail.com>
2021-09-14 10:30:10 +0900

    error types

--- client/src/PPWAdmin/modules/errorlogs/redux/reducers.js
+++ client/src/PPWAdmin/modules/errorlogs/redux/reducers.js
@@ -6,13 +6,15 @@ import { getErrorLogs } from "./services";
 export const actionTypes = {
     getErrorLogs: "Get Error Logs",
     getErrorLogsSuccess: "Get Error Logs Success",
+    onErrorLogNameChange: "On Error Log Name Change",
 };
 
 const initialState = {
     errorlogs: [],
     loading: false,
     currentPage: 1,
-    total: 0
+    total: 0,
+    name: '',
 };
 
 export const reducer = persistReducer(
@@ -34,16 +36,21 @@ export const reducer = persistReducer(
 export const actions = {
     getErrorLogs: (page = 1) => ({ type: actionTypes.getErrorLogs, page }),
     getErrorLogsSuccess: (data) => ({ type: actionTypes.getErrorLogsSuccess, data }),
+    onErrorLogNameChange: (name) => ({ type: actionTypes.onErrorLogNameChange, name }),
 };
 
 export function* saga() {
     yield takeLatest(actionTypes.getErrorLogs, function* getErrorLogsSaga() {
         try {
             const state = yield select((state) => state.errorlogs);
-            const { data } = yield getErrorLogs(state.page);
+            const { data } = yield getErrorLogs(state.page, state.name);
             yield put(actions.getErrorLogsSuccess({ errorlogs: data.data, total: data.total }));
         } catch (error) {
             yield put(actions.getErrorLogsSuccess({ errorlogs: [], total: 0 }));
         }
     });
+
+    yield takeLatest(actionTypes.onErrorLogNameChange, function* () {
+        yield put(actions.getErrorLogs());
+    })
 }
