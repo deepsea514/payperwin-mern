aada434f5bc3696b8a8a952d7c70f4cc6d66ec7d
Black Storm <nightyellowstormy@gmail.com>
2021-09-15 14:57:38 +0900

    mobile issue

--- client/src/components/bet.jsx
+++ client/src/components/bet.jsx
@@ -65,13 +65,13 @@ class Bet extends PureComponent {
         const { oddsFormat } = this.props;
         switch (oddsFormat) {
             case 'decimal':
-                if (odd > 0) {
+                if (odd > 0)
                     return Number(1 + odd / 100).toFixed(2);
-                }
-                else {
-                    return Number(1 - 100 / odd);
-                }
+                return Number(1 - 100 / odd).toFixed(2);
             case 'american':
+                if (odd > 0)
+                    return '+' + odd;
+                return odd;
             default:
                 return odd;
         }
