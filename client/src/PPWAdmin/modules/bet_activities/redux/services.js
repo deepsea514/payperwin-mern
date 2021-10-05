af4ba07bf376e824eecdb0bf655bd0e22a054b72
Snow White <neoprince1992@gmail.com>
2021-10-05 14:59:39 +0900

    add total players in dashboard

--- client/src/PPWAdmin/modules/bet_activities/redux/services.js
+++ client/src/PPWAdmin/modules/bet_activities/redux/services.js
@@ -3,23 +3,15 @@ import _env from '../../../../env.json';
 const serverUrl = _env.appAdminUrl;
 
 export function getBetActivities(page, filter, perPage = null) {
-    let url = `${serverUrl}/bets?page=${page}`;
-    if (perPage) url += `&perPage=${perPage}`;
-    const { datefrom, dateto, sport, status, minamount, maxamount, house, match } = filter;
-    if (datefrom) url += `&datefrom=${encodeURIComponent(new Date(datefrom).toISOString())}`;
-    if (dateto) url += `&dateto=${encodeURIComponent(new Date(dateto).toISOString())}`;
-    if (sport) url += `&sport=${encodeURIComponent(sport)}`;
-    if (status) url += `&status=${encodeURIComponent(status)}`;
-    if (minamount) url += `&minamount=${encodeURIComponent(minamount)}`;
-    if (maxamount) url += `&maxamount=${encodeURIComponent(maxamount)}`;
-    if (house) url += `&house=${encodeURIComponent(house)}`;
-    if (match) url += `&match=${encodeURIComponent(match)}`;
-
-    return axios.get(url, { withCredentials: true });
+    return axios.get(`${serverUrl}/bets`, {
+        withCredentials: true, params: {
+            page, perPage, ...filter
+        }
+    });
 }
 
 export function getBetDetail(id) {
-    return axios.get(`${serverUrl}/bet?id=${id}`, { withCredentials: true });
+    return axios.get(`${serverUrl}/bet`, { withCredentials: true, params: { id } });
 }
 
 export function getSports() {
@@ -27,15 +19,7 @@ export function getSports() {
 }
 
 export function getWagerActivityAsCSV(filter) {
-    let url = `${serverUrl}/bets-csv?format=csv`;
-    const { datefrom, dateto, sport, minamount, maxamount } = filter;
-    if (datefrom) url += `&datefrom=${encodeURIComponent(new Date(datefrom).toISOString())}`;
-    if (dateto) url += `&dateto=${encodeURIComponent(new Date(dateto).toISOString())}`;
-    if (sport) url += `&sport=${encodeURIComponent(sport)}`;
-    if (minamount) url += `&minamount=${encodeURIComponent(minamount)}`;
-    if (maxamount) url += `&maxamount=${encodeURIComponent(maxamount)}`;
-
-    return axios.get(url, { withCredentials: true });
+    return axios.get(`${serverUrl}/bets-csv`, { withCredentials: true, params: filter });
 }
 
 export function deleteBet(id) {
