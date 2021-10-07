81c3b8b7b09f3f99fbe256a41ccd390c36b68cb4
Black Storm <nightyellowstormy@gmail.com>
2021-10-07 10:22:11 +0900

    update event odds

--- client/src/helpers/calculateNewOdds.js
+++ client/src/helpers/calculateNewOdds.js
@@ -1,6 +1,20 @@
 function calculateNewOdds(home, away) {
     const moneylineDifference = Math.abs(Math.abs(home) - Math.abs(away)) / 2;
     let bigHome = 1;
+
+    if (home == away) {
+        if (home < 0 && home + 5.5 > -100) {
+            return {
+                newHome: 200 + parseInt(home) + 5.5,
+                newAway: 200 + parseInt(away) + 5.5,
+            }
+        }
+        return {
+            newHome: parseInt(home) + 5.5,
+            newAway: parseInt(away) + 5.5,
+        }
+    }
+
     if (home > 0 && away > 0 || home < 0 && away < 0) {
         const newHome = (home + away) / 2;
         const newAway = (home + away) / 2;
--- server/libs/calculateNewOdds.js
+++ server/libs/calculateNewOdds.js
@@ -2,8 +2,15 @@ const calculateNewOdds = (home, away, pick, type, subtype = null) => {
     if (subtype != null && ['alternative_total', 'alternative_spread'].includes(type)) {
         return pick == 'home' ? home : away;
     }
-    if (['alternative_spread', 'alternative_total'].includes(type)) return pick == 'home' ? home : away;
     const moneylineDifference = Math.abs(Math.abs(home) - Math.abs(away)) / 2;
+
+    if (home == away) {
+        if (home < 0 && home + 5.5 > -100) {
+            return 200 + parseInt(home) + 5.5;
+        }
+        return parseInt(home) + 5.5;
+    }
+
     let bigHome = 1;
     if (home > 0 && away > 0 || home < 0 && away < 0) {
         return parseInt((home + away) / 2);
