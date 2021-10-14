61d60b8bee8bfae584bed0662dfb0c8fcee88f42
Snow White <neoprince1992@gmail.com>
2021-10-14 11:25:08 +0900

    make nhl top

--- server/dataCollector/bet365/arrangeLeagues.js
+++ server/dataCollector/bet365/arrangeLeagues.js
@@ -15,6 +15,7 @@ const getTopLeagueInSport = (sport) => {
             case "Snooker":
             case "Darts":
             case "Ice Hockey":
+                return 'NHL'
             case "Rugby League":
             case "Australian Rules":
             case "Bowls":
