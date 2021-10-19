3d9c719d8347efb023306aa0e67584941e647928
Black Storm <nightyellowstormy@gmail.com>
2021-10-20 08:36:52 +0900

    get cricket score

---
+++ server/dataCollector/bet365/getMatchScores/getCricketMatchScores.js
@@ -0,0 +1,24 @@
+const getTotal = (score) => {
+    score = score.split('/');
+    return score[0];
+}
+
+const getCricketMatchScores = (type, subtype, ss, scores, time_status) => {
+    if (time_status == "1") return 'inplay';
+    //107/3(15.1)-106/10(20)
+    const matchResult = {
+        homeScore: 0,
+        awayScore: 0,
+    }
+    try {
+        const scores = ss.split('-');
+        matchResult.homeScore += Number(getTotal(scores[0]));
+        matchResult.awayScore += Number(getTotal(scores[1]));
+        return matchResult;
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+}
+
+module.exports = getCricketMatchScores;
