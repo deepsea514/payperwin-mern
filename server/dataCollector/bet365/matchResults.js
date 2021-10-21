5d77ed121917a5a7e2de7867b043318d7ce95b26
Snow White <neoprince1992@gmail.com>
2021-10-21 11:56:06 +0900

    get lines

--- server/dataCollector/bet365/matchResults.js
+++ server/dataCollector/bet365/matchResults.js
@@ -12,7 +12,7 @@ const matchResults = async () => {
     }
     const { bet365ApiKey } = bet365Addon.value;
 
-    // matchResultsP2PAndSB(bet365ApiKey);
+    matchResultsP2PAndSB(bet365ApiKey);
     matchResultsParlay(bet365ApiKey);
 }
 
