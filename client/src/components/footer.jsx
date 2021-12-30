b254b76b966627feb56326b01dc20bc2361c3e0c
Snow <neoprince1992@gmail.com>
2021-12-30 14:42:33 +0900

    make frontend changes

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -48,17 +48,17 @@ const Footer = (props) => {
                         </div>
                     </div>
                     <div className="glambing">
-                        <a href="#" className="min-age">18+</a><FormattedMessage id="COMPONENTS.GAMBLING.CANBE.ADDICTIVE" />
+                        <a href="#" className="min-age">19+</a><FormattedMessage id="COMPONENTS.GAMBLING.CANBE.ADDICTIVE" />
                     </div>
-                    <div className="payment_container">
+                    {/* <div className="payment_container">
                         <span><img alt="Visa, Master Card" title="Visa, Master Card" src="/images/credit-card-gray.png" /></span>
                         <span><img alt="Etransfer" title="Etransfer" src="/images/eTransfer-gray.png" /></span>
                         <span><img alt="Bitcoin" title="Bitcoin" src="/images/bitcoin-gray.png" /></span>
                         <span><img alt="Ethereum" title="Ethereum" src="/images/Ethereum-gray.png" /></span>
                         <span><img alt="Tether" title="Tether" src="/images/USDT-gray.png" /></span>
-                    </div>
+                    </div> */}
                     <p>©2021 PAYPER WIN</p>
-                    <div className="d-flex justify-content-between pt-3">
+                    {/* <div className="d-flex justify-content-between pt-3">
                         <a href="#" >
                             <img alt="eCogra" src="/images/ecogra.png" style={{ maxHeight: '32px' }} />
                         </a>
@@ -71,7 +71,7 @@ const Footer = (props) => {
                         <a href="#">
                             <img alt="Network Solution" src="/images/networksolution.png" style={{ maxHeight: '32px' }} />
                         </a>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </footer>
