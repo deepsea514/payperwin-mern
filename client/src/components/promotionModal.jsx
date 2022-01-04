f4c39002338c04285ac65d0c0429f09ef7c767b0
Snow <neoprince1992@gmail.com>
2022-01-04 15:02:45 +0900

    remove console

--- client/src/components/promotionModal.jsx
+++ client/src/components/promotionModal.jsx
@@ -37,9 +37,9 @@ export default class PromotionModal extends Component {
                         keyboardNavigation
                         defaultInterval={10000}
                         stories={stories}
-                        onStoryEnd={(s, st) => console.log('story ended', s, st)}
-                        onAllStoriesEnd={(s, st) => console.log('all stories ended', s, st)}
-                        onStoryStart={(s, st) => console.log('story started', s, st)}
+                        // onStoryEnd={(s, st) => console.log('story ended', s, st)}
+                        // onAllStoriesEnd={(s, st) => console.log('all stories ended', s, st)}
+                        // onStoryStart={(s, st) => console.log('story started', s, st)}
                         storyContainerStyles={{ width: '100%', height: '100%' }}
                         storyStyles={{ width: '100%', height: '100%' }}
                         width='100%'
