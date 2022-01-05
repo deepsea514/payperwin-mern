edc8492387612a45b1b51b074215f8604a793857
Black Storm <nightyellowstormy@gmail.com>
2022-01-05 10:41:52 +0900

    make promotion mockup

--- client/src/components/promotionModal.jsx
+++ client/src/components/promotionModal.jsx
@@ -1,195 +1,7 @@
-import React, { Component, useRef, useState, useEffect } from "react";
+import React, { Component } from "react";
 import Stories from 'react-insta-stories';
 import SVG from 'react-inlinesvg'
-
-const imageContent = ({ story, action, isPaused, config, closePromotion }) => {
-    const [loaded, setLoaded] = useState(false);
-    const { width, height, loader, storyStyles } = config;
-    let computedStyles = {
-        ...styles.storyContent,
-        ...(storyStyles || {}),
-        width: '100%',
-        display: 'block',
-        height: 'auto',
-        maxHeight: 'auto',
-        position: 'absolute',
-        top: '-100%',
-        left: 0,
-        right: 0,
-        bottom: '-100%',
-        margin: 'auto'
-    }
-
-    const imageLoaded = () => {
-        setLoaded(true);
-        action('play');
-    }
-
-    return (
-        <>
-            <div style={{
-                width: width,
-                height: height,
-                position: 'relative',
-                overflow: 'hidden'
-            }}>
-                <img style={computedStyles} src={story.url} onLoad={imageLoaded} />
-                {!loaded && (
-                    <div style={{
-                        width: width,
-                        height: height,
-                        position: "absolute",
-                        left: 0,
-                        top: 0,
-                        background: "rgba(0, 0, 0, 0.9)",
-                        zIndex: 9,
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        color: "#ccc"
-                    }}>
-                        {loader || <SVG src="/images/puff.svg" />}
-                    </div>
-                )}
-            </div>
-            <div style={{ position: "absolute", right: 12, top: 20, zIndex: 19 }}>
-                <div style={styles.header}>
-                    <div style={styles.close} onClick={closePromotion}>
-                        <i className="fa fa-times" style={styles.closeIcon} />
-                    </div>
-                </div>
-            </div>
-        </>
-    )
-}
-
-const videoContent = ({ story, action, isPaused, config, messageHandler, closePromotion }) => {
-    const [loaded, setLoaded] = useState(false);
-    const [muted, setMuted] = useState(false);
-    const { width, height, loader, storyStyles } = config;
-
-    let computedStyles = {
-        ...styles.storyContent,
-        ...(storyStyles || {})
-    }
-
-    let vid = useRef(null);
-
-    useEffect(() => {
-        if (vid.current) {
-            if (isPaused) {
-                vid.current.pause();
-            } else {
-                vid.current.play().catch(() => { });
-            }
-        }
-    }, [isPaused]);
-
-    const onWaiting = () => {
-        action("pause", true);
-    }
-
-    const onPlaying = () => {
-        action("play", true);
-    }
-
-    const videoLoaded = () => {
-        messageHandler('UPDATE_VIDEO_DURATION', { duration: vid.current.duration });
-        setLoaded(true);
-        vid.current.play().then(() => {
-            action('play');
-        }).catch(() => {
-            setMuted(true);
-            vid.current.play().finally(() => {
-                action('play');
-            })
-        });
-    }
-
-    return (
-        <>
-            <div style={{
-                ...styles.videoContainer, ...{
-                    width: width,
-                    height: height,
-                }
-            }}>
-                <video
-                    ref={vid}
-                    style={computedStyles}
-                    src={story.url}
-                    controls={false}
-                    onLoadedData={videoLoaded}
-                    playsInline
-                    onWaiting={onWaiting}
-                    onPlaying={onPlaying}
-                    muted={muted}
-                    autoPlay
-                    webkit-playsinline="true"
-                />
-                {!loaded && (
-                    <div
-                        style={{
-                            width: width,
-                            height: height,
-                            position: "absolute",
-                            left: 0,
-                            top: 0,
-                            background: "rgba(0, 0, 0, 0.9)",
-                            zIndex: 9,
-                            display: "flex",
-                            justifyContent: "center",
-                            alignItems: "center",
-                            color: "#ccc"
-                        }}
-                    >
-                        {loader || <SVG src="/images/puff.svg" />}
-                    </div>
-                )}
-            </div>
-            <div className="header-close" style={{ position: "absolute", right: 12, top: 20, zIndex: 19 }}>
-                <div style={styles.header}>
-                    <div style={styles.close} onClick={closePromotion}>
-                        <i className="fa fa-times" style={styles.closeIcon} />
-                    </div>
-                </div>
-            </div>
-        </>
-    )
-}
-
-const styles = {
-    storyContent: {
-        width: "auto",
-        maxWidth: "100%",
-        maxHeight: "100%",
-        margin: "auto"
-    },
-    header: {
-        display: 'flex',
-        alignItems: 'center'
-    },
-    close: {
-        width: 40,
-        height: 40,
-        borderRadius: 40,
-        filter: 'drop-shadow(0 0px 2px rgba(0, 0, 0, 0.5))',
-        display: 'flex',
-        alignItems: 'center',
-        background: '#0004'
-    },
-    closeIcon: {
-        margin: 'auto',
-        fontSize: '20px',
-        padding: '0',
-        color: 'white'
-    },
-    videoContainer: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
-}
+import { ImageRenderer, ImageTester, VideoRenderer, VideoTester } from "../libs/promotionRenderer";
 
 export default class PromotionModal extends Component {
     constructor(props) {
@@ -198,32 +10,39 @@ export default class PromotionModal extends Component {
             stories: null,
             loading: false,
         }
+
+        const { closePromotion } = props;
+        this.renderers = [
+            {
+                renderer: (props) => ImageRenderer({ ...props, closePromotion }),
+                tester: ImageTester
+            },
+            {
+                renderer: (props) => VideoRenderer({ ...props, closePromotion }),
+                tester: VideoTester
+            }
+        ]
     }
 
     componentDidMount() {
-        const { closePromotion } = this.props;
         this.setState({ loading: true });
         setTimeout(() => this.setState({
             stories: [
                 {
                     url: '/images/promotion-banner.jpg',
-                    content: (params) => imageContent({ ...params, closePromotion }),
-                    loader: true,
+                    type: 'image',
                 },
                 {
                     url: 'https://placeimg.com/640/480',
-                    content: (params) => imageContent({ ...params, closePromotion }),
-                    loader: true,
+                    type: 'image',
                 },
                 {
-                    url: 'https://placeimg.com/480/640',
-                    content: (params) => imageContent({ ...params, closePromotion }),
-                    loader: true,
+                    url: '/media/demo video.mp4',
+                    type: 'video',
                 },
                 {
                     url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4',
-                    // type: 'video',
-                    content: (params) => videoContent({ ...params, closePromotion }),
+                    type: 'video',
                 }
             ],
             loading: false,
@@ -232,6 +51,7 @@ export default class PromotionModal extends Component {
 
     render() {
         const { stories, loading } = this.state;
+
         return (
             <div className="modal promotion">
                 {stories && <Stories
@@ -242,6 +62,7 @@ export default class PromotionModal extends Component {
                     width='100%'
                     height='100%'
                     preventDefault
+                    renderers={this.renderers}
                 />}
                 {loading && <>
                     <div style={{
---
+++ client/src/libs/promotionRenderer.js
@@ -0,0 +1,204 @@
+import React, { useRef, useState, useEffect } from "react";
+import SVG from 'react-inlinesvg'
+
+export const ImageRenderer = ({ story, action, isPaused, config, closePromotion }) => {
+    const [loaded, setLoaded] = useState(false);
+    const { width, height, loader, storyStyles } = config;
+    let computedStyles = {
+        ...styles.storyContent,
+        ...(storyStyles || {}),
+        width: '100%',
+        display: 'block',
+        height: 'auto',
+        maxHeight: 'auto',
+        position: 'absolute',
+        top: '-100%',
+        left: 0,
+        right: 0,
+        bottom: '-100%',
+        margin: 'auto'
+    }
+
+    const imageLoaded = () => {
+        setLoaded(true);
+        action('play');
+    }
+
+    return (
+        <>
+            <div style={{
+                width: width,
+                height: height,
+                position: 'relative',
+                overflow: 'hidden'
+            }}>
+                <img style={computedStyles} src={story.url} onLoad={imageLoaded} />
+                {!loaded && (
+                    <div style={{
+                        width: width,
+                        height: height,
+                        position: "absolute",
+                        left: 0,
+                        top: 0,
+                        background: "rgba(0, 0, 0, 0.9)",
+                        zIndex: 9,
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        color: "#ccc"
+                    }}>
+                        {loader || <SVG src="/images/puff.svg" />}
+                    </div>
+                )}
+            </div>
+            <div style={{ position: "absolute", right: 12, top: 20, zIndex: 19 }}>
+                <div style={styles.header}>
+                    <div style={styles.close} onClick={closePromotion}>
+                        <i className="fa fa-times" style={styles.closeIcon} />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+export const ImageTester = (story) => {
+    return {
+        condition: !story.content && (!story.type || story.type === 'image'),
+        priority: 3
+    }
+}
+
+export const VideoRenderer = ({ story, action, isPaused, config, messageHandler, closePromotion }) => {
+    const [loaded, setLoaded] = useState(false);
+    const [muted, setMuted] = useState(false);
+    const { width, height, loader, storyStyles } = config;
+
+    let computedStyles = {
+        ...styles.storyContent,
+        ...(storyStyles || {})
+    }
+
+    let vid = useRef(null);
+
+    useEffect(() => {
+        if (vid.current) {
+            if (isPaused) {
+                vid.current.pause();
+            } else {
+                vid.current.play().catch(() => { });
+            }
+        }
+    }, [isPaused]);
+
+    const onWaiting = () => {
+        action("pause", true);
+    }
+
+    const onPlaying = () => {
+        action("play", true);
+    }
+
+    const videoLoaded = () => {
+        const res = messageHandler('UPDATE_VIDEO_DURATION', { duration: vid.current.duration });
+        setLoaded(true);
+        vid.current.play().then(() => {
+            action('play');
+        }).catch(() => {
+            setMuted(true);
+            vid.current.play().finally(() => {
+                action('play');
+            })
+        });
+    }
+
+    return (
+        <>
+            <div style={{
+                ...styles.videoContainer, ...{
+                    width: width,
+                    height: height,
+                }
+            }}>
+                <video
+                    ref={vid}
+                    style={computedStyles}
+                    src={story.url}
+                    controls={false}
+                    onLoadedData={videoLoaded}
+                    playsInline
+                    onWaiting={onWaiting}
+                    onPlaying={onPlaying}
+                    muted={muted}
+                    autoPlay
+                    webkit-playsinline="true"
+                />
+                {!loaded && (
+                    <div
+                        style={{
+                            width: width,
+                            height: height,
+                            position: "absolute",
+                            left: 0,
+                            top: 0,
+                            background: "rgba(0, 0, 0, 0.9)",
+                            zIndex: 9,
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            color: "#ccc"
+                        }}
+                    >
+                        {loader || <SVG src="/images/puff.svg" />}
+                    </div>
+                )}
+            </div>
+            <div className="header-close" style={{ position: "absolute", right: 12, top: 20, zIndex: 19 }}>
+                <div style={styles.header}>
+                    <div style={styles.close} onClick={closePromotion}>
+                        <i className="fa fa-times" style={styles.closeIcon} />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export const VideoTester = (story) => {
+    return {
+        condition: story.type === 'video',
+        priority: 3
+    }
+}
+
+const styles = {
+    storyContent: {
+        width: "auto",
+        maxWidth: "100%",
+        maxHeight: "100%",
+        margin: "auto"
+    },
+    header: {
+        display: 'flex',
+        alignItems: 'center'
+    },
+    close: {
+        width: 40,
+        height: 40,
+        borderRadius: 40,
+        filter: 'drop-shadow(0 0px 2px rgba(0, 0, 0, 0.5))',
+        display: 'flex',
+        alignItems: 'center',
+        background: '#0004'
+    },
+    closeIcon: {
+        margin: 'auto',
+        fontSize: '20px',
+        padding: '0',
+        color: 'white'
+    },
+    videoContainer: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+}
