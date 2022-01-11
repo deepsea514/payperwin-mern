170b11bcf2d5b2fe6d6274afd8795702328e0611
Black Storm <nightyellowstormy@gmail.com>
2022-01-11 10:30:59 +0900

    sort leagues

--- server/libs/sortSearchResults.js
+++ server/libs/sortSearchResults.js
@@ -1,40 +1,56 @@
+const leaguesOrder = {
+    '10041830': { order: 0 }, // NBA
+    '10037477': { order: 1 }, // NHL
+    '10037219': { order: 3 }, // NFL
+    '10037485': { order: 4 }, // MLB
+    '10036983': { order: 5 }, // UFC
+    '10041809': { order: 6 }, // UEFA C
+    '10041957': { order: 7 }, // UEFA L
+}
+
 const sortsOrder = {
     "American Football": {
-        order: 0,
-        leagues: {
-            '10037219': { order: 0 }
-        }
+        order: 0
     },
     "Basketball": {
-        order: 1,
-        leagues: {
-            '10041830': { order: 0 }
-        }
+        order: 1
     },
     "Baseball": {
         order: 2,
-        leagues: {
-            '10037485': { order: 0 }
-        }
     },
     "Ice Hockey": {
         order: 3,
-        leagues: {
-            '10037477': { order: 0 }
-        }
+    },
+    "Boxing-UFC": {
+        order: 4,
     },
     "Soccer": {
-        order: 4,
-        leagues: {
-            '10041809': { order: 0 },
-            '10041957': { order: 1 },
-        }
+        order: 5,
     }
 }
 
 const sortSearchResults = (results) => {
     return results.sort((a, b) => {
-        if (a.sportName == b.sportName) {
+        if (a.sportName != b.sportName) {
+            const leagueOrderA = leaguesOrder[a.leagueId ? a.leagueId : a.originId];
+            const leagueOrderB = leaguesOrder[b.leagueId ? b.leagueId : b.originId];
+            if (leagueOrderA && leagueOrderB) {
+                return leagueOrderA.order - leagueOrderB.order;
+            } else if (leagueOrderA) {
+                return -1;
+            } else if (leagueOrderB) {
+                return 1;
+            }
+
+            const orderA = sortsOrder[a.sportName];
+            const orderB = sortsOrder[b.sportName];
+            if (orderA && orderB) {
+                return orderA.order - orderB.order;
+            } else if (orderB) {
+                return 1;
+            }
+            return -1;
+        } else {
             const sport = sortsOrder[a.sportName];
             if (!sport) return 0;
             const orderA = sport.leagues[a.leagueId ? a.leagueId : a.originId];
@@ -45,17 +61,10 @@ const sortSearchResults = (results) => {
                 return 1;
             }
             return -1;
-        } else {
-            const orderA = sortsOrder[a.sportName];
-            const orderB = sortsOrder[b.sportName];
-            if (orderA && orderB) {
-                return orderA.order - orderB.order;
-            } else if (orderB) {
-                return 1;
-            }
-            return -1;
         }
     });
 }
 
-module.exports = sortSearchResults;
+module.exports = {
+    sortSearchResults
+};
