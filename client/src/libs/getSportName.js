8d7b43cb57a0b6bdae19325513f79f7d25115339
Snow <neoprince1992@gmail.com>
2022-01-14 16:09:49 +0900

    update git sportname

--- client/src/libs/getSportName.js
+++ client/src/libs/getSportName.js
@@ -2,12 +2,12 @@ const sportsData = require('../libs/sports.json');
 
 const getSportName = (shortName) => {
     const sport = sportsData.find(sport => sport.shortName == shortName);
-    return sport?.name;
+    return sport ? sport.name : '';
 }
 
 const getShortSportName = (name) => {
     const sport = sportsData.find(sport => sport.name == name);
-    return sport?.shortName;
+    return sport ? sport.shortName : '';
 }
 
 module.exports = {
