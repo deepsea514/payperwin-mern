385e35035250f47f030b65a741a0922349e46b73
Black Storm <nightyellowstormy@gmail.com>
2022-01-19 15:38:24 +0900

    change basic help modal and auto accept sportsbook

--- client/src/components/basicmodal.jsx
+++ client/src/components/basicmodal.jsx
@@ -1,25 +1,38 @@
 import React from "react";
 
 export default class BasicModal extends React.Component {
+    getTitle = () => {
+        const { showHelp } = this.props;
+        switch (showHelp) {
+            case 'moneyline':
+                return 'MONEYLINE';
+            case 'spread':
+                return 'POINT SPREAD';
+            case 'total':
+            default:
+                return 'TOTAL POINTS';
+        }
+    }
+
     render() {
-        const { onClose } = this.props;
+        const { onClose, showHelp } = this.props;
         return (
             <div className="modal confirmation">
                 <div className="background-closer bg-modal" onClick={onClose} />
                 <div className="col-in">
                     <i className="fal fa-times" style={{ cursor: 'pointer' }} onClick={onClose} />
                     <div>
-                        <b>BET TYPE</b>
+                        <b>{this.getTitle()}</b>
                         <hr />
-                        <p>
-                            <b>MONEYLINE:</b> The money line is simply choosing the outright winner, which team will win the the game.
-                        </p>
-                        <p>
-                            <b>POINT SPREAD:</b> The spread, also referred to as the line, is used to even the odds between two unevenly matched teams.
-                        </p>
-                        <p>
-                            <b>TOTAL POINTS:</b> This is a bet on the total number of points scored by both teams.
-                        </p>
+                        {showHelp == 'moneyline' && <p>
+                            The money line is simply choosing the outright winner, which team will win the the game.
+                        </p>}
+                        {showHelp == 'spread' && <p>
+                            The spread, also referred to as the line, is used to even the odds between two unevenly matched teams.
+                        </p>}
+                        {showHelp == 'total' && <p>
+                            This is a bet on the total number of points scored by both teams.
+                        </p>}
                         <div className="text-right">
                             <button className="form-button ml-2" onClick={onClose}> OK </button>
                         </div>
