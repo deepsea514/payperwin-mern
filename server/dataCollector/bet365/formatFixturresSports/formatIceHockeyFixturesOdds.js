4ae31dac09343dc3d6a3bcee5f69ea6c72c41154
Snow <neoprince1992@gmail.com>
2022-02-02 13:24:57 +0900

    got hockey and tennis team totals

--- server/dataCollector/bet365/formatFixturresSports/formatIceHockeyFixturesOdds.js
+++ server/dataCollector/bet365/formatFixturresSports/formatIceHockeyFixturesOdds.js
@@ -1,6 +1,6 @@
 const { convertDecimalToAmericanOdds } = require('../convertOdds');
 const formatIceHockeyFixturesOdds = (event) => {
-    const { main, schedule } = event.odds;
+    const { main, others } = event.odds;
     let line = {
         originId: event.id,
         endDate: new Date(parseInt(event.time) * 1000),
@@ -8,6 +8,8 @@ const formatIceHockeyFixturesOdds = (event) => {
         moneyline: null,
         spreads: [],
         totals: [],
+        home_totals: [],
+        away_totals: [],
     }
 
     if (main) {
@@ -41,11 +43,63 @@ const formatIceHockeyFixturesOdds = (event) => {
         }
     }
 
+    if (others) {
+        let team_total_2_way = others.find(other => other.sp.team_total_2_way);
+        if (team_total_2_way && team_total_2_way.sp.team_total_2_way) {
+            team_total_2_way = team_total_2_way.sp.team_total_2_way.odds;
+            let home_totals = team_total_2_way.filter(total => total.header == "1");
+            let away_totals = team_total_2_way.filter(total => total.header == "2");
+            while (home_totals.length > 0) {
+                const first = home_totals[0];
+                const first_options = first.handicap.split(' ');
+                const over_under = first_options[0];
+                const points = first_options[1];
+                const second = home_totals.find(total => total.handicap == (over_under == 'Over' ? 'Under ' : 'Over ') + points);
+                if (!second) {
+                    home_totals = home_totals.filter(total => total.id != first.id);
+                    continue;
+                }
+                const over = over_under == 'Over' ? first : second;
+                const under = over_under == 'Under' ? first : second;
+                line.home_totals.push({
+                    altLineId: over.id,
+                    points: Number(points),
+                    over: convertDecimalToAmericanOdds(over.odds),
+                    under: convertDecimalToAmericanOdds(under.odds),
+                });
+                home_totals = home_totals.filter(total => total.id != over.id && total.id != under.id);
+            }
+
+            while (away_totals.length > 0) {
+                const first = away_totals[0];
+                const first_options = first.handicap.split(' ');
+                const over_under = first_options[0];
+                const points = first_options[1];
+                const second = away_totals.find(total => total.handicap == (over_under == 'Over' ? 'Under ' : 'Over ') + points);
+                if (!second) {
+                    away_totals = away_totals.filter(total => total.id != first.id);
+                    continue;
+                }
+                const over = over_under == 'Over' ? first : second;
+                const under = over_under == 'Under' ? first : second;
+                line.away_totals.push({
+                    altLineId: over.id,
+                    points: Number(points),
+                    over: convertDecimalToAmericanOdds(over.odds),
+                    under: convertDecimalToAmericanOdds(under.odds),
+                });
+                away_totals = away_totals.filter(total => total.id != over.id && total.id != under.id);
+            }
+        }
+    }
+
     if (line.moneyline && (!line.moneyline.home || !line.moneyline.away)) {
         line.moneyline = null
     }
     line.spreads = line.spreads && line.spreads.length ? line.spreads : null;
     line.totals = line.totals && line.totals.length ? line.totals : null;
+    line.home_totals = line.home_totals && line.home_totals.length ? line.home_totals : null;
+    line.away_totals = line.away_totals && line.away_totals.length ? line.away_totals : null;
 
     if (line.moneyline || line.spreads || line.totals)
         return line;
