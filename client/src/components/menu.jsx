91c082f3ba95ba7759b3beeafd82c3f4d66c41b6
Black Storm <nightyellowstormy@gmail.com>
2022-02-02 15:41:39 +0900

    show history in account

--- client/src/components/menu.jsx
+++ client/src/components/menu.jsx
@@ -133,12 +133,6 @@ class Menu extends Component {
                                         <i className="fas fa-chevron-right float-right" />
                                     </a>
                                 </li>
-                                <li className={`nav-item col-6 ${pathname === '/history' ? 'active' : ''}`}>
-                                    <Link to={{ pathname: '/history' }} className="nav-link" onClick={() => toggleField('menuOpen')}>
-                                        <i className="fas fa-history" />
-                                        <span><FormattedMessage id="COMPONENTS.SIDEBAR.BETTING_HISTORY" /></span>
-                                    </Link>
-                                </li>
                                 <li className="nav-item col-6">
                                     <a href="https://shop.payperwin.com" className="nav-link" target="_blank">
                                         <i className="fas fa-money-check" />
@@ -298,6 +292,14 @@ class Menu extends Component {
                                         onClick={() => toggleField('menuOpen')}>
                                         <i className="fas fa-gamepad" />
                                         <FormattedMessage id="COMPONENTS.SIDEBAR.OPENBETS" />
+                                    </Link>
+                                </li>
+                                <li className={`nav-item col-6 ${pathname === '/history' ? 'active' : ''}`}>
+                                    <Link to={{ pathname: '/history' }}
+                                        className="nav-link"
+                                        onClick={() => toggleField('menuOpen')}>
+                                        <i className="fas fa-history" />
+                                        <span><FormattedMessage id="COMPONENTS.SIDEBAR.BETTING_HISTORY" /></span>
                                     </Link>
                                 </li>
                                 <li className={`nav-item ${pathname === '/preferences' ? 'active' : ''}`}>
