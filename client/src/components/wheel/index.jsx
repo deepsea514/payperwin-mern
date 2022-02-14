0a3e805b169fd2b2d0d7bdfe32039bd4acf4375f
Snow <neoprince1992@gmail.com>
2022-02-14 10:05:03 +0900

    update prize pool

---
+++ client/src/components/wheel/index.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import rouletteSelection from "../../libs/rouletteSelection";
+import "./index.css";
+
+export default class Wheel extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { selectedItem: null };
+    }
+
+    selectItem = () => {
+        const { selectedItem } = this.state;
+        const { items, onSelectItem } = this.props;
+        if (selectedItem === null) {
+            const selectedItem = rouletteSelection(items);
+            if (onSelectItem) {
+                setTimeout(() => onSelectItem(selectedItem), 6000);
+            }
+            this.setState({ selectedItem });
+        }
+    }
+
+    render() {
+        const { selectedItem } = this.state;
+        const { items } = this.props;
+
+        const wheelVars = {
+            "--nb-item": items.length,
+            "--selected-item": selectedItem
+        };
+        const spinning = selectedItem !== null ? "spinning" : "";
+
+        return (
+            <div className="wheel-container">
+                <div className={`wheel ${spinning}`}
+                    style={wheelVars}
+                    onClick={this.selectItem}>
+                    {items.map((item, index) => (
+                        <div className="wheel-item"
+                            key={index}
+                            style={{ "--item-nb": index }}>
+                            {item.text}
+                        </div>
+                    ))}
+                </div>
+            </div>
+        );
+    }
+}
