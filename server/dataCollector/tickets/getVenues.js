e2bd29a8ec36a8876b84129364b9faae05a74487
Black Storm <nightyellowstormy@gmail.com>
2022-03-15 12:25:18 +0900

    get venues detail

--- server/dataCollector/tickets/getVenues.js
+++ server/dataCollector/tickets/getVenues.js
@@ -1,31 +1,47 @@
 const TevoClient = require('ticketevolution-node');
+const fs = require('fs');
 const TicketVenue = require('../../models/ticket_venue');
+const caVenues = require('./venues_ca.json');
+const usVenues = require('./venues_us.json');
+const GET_FROM_API = false;
 
 const getVenues = async (API_TOKEN, API_SECRET) => {
-    const tevoClient = new TevoClient({
-        apiToken: API_TOKEN,
-        apiSecretKey: API_SECRET,
-    });
-    try {
-        let page = 1;
-        do {
-            const response = await tevoClient.getJSON('http://api.sandbox.ticketevolution.com/v9/venues?per_page=1000&page=' + page);
-            if (response.venues && response.venues.length) {
-                const venues_res = response.venues;
-                for (const venue of venues_res) {
-                    await TicketVenue.findOneAndUpdate({ id: venue.id }, venue, { upsert: true });
+    if (GET_FROM_API) {
+        const tevoClient = new TevoClient({
+            apiToken: API_TOKEN,
+            apiSecretKey: API_SECRET,
+        });
+        try {
+            let venues = [];
+            let page = 1;
+            do {
+                const response = await tevoClient.getJSON('http://api.sandbox.ticketevolution.com/v9/venues?per_page=1000&page=' + page);
+                if (response.venues && response.venues.length) {
+                    const venues_res = response.venues;
+                    venues = [...venues, ...venues_res]
+                } else {
+                    break;
                 }
-            } else {
-                break;
+                console.log('Got Venues, Page => ', page, response.total_entries);
+                page++;
+                if (response.total_entries && response.total_entries < response.current_page * response.per_page)
+                    break;
+            } while (true);
+            fs.writeFileSync('venues.json', JSON.stringify(venues));
+            console.log('Got all Venues.');
+        } catch (error) {
+            console.error(error);
+        }
+    } else {
+        for (const venue of [...caVenues, ...usVenues]) {
+            try {
+                await TicketVenue.findOneAndUpdate({ id: venue.id }, venue, { upsert: true });
+            } catch (error) {
+                console.error(error);
             }
-            console.log('Got Venues, Page => ', page, response.total_entries);
-            page++;
-            if (response.total_entries && response.total_entries < response.current_page * response.per_page)
-                break;
-        } while (true);
-        console.log('Got Venues.');
-    } catch (error) {
-        console.error(error);
+            delete venue._id;
+        }
+        console.log('Got all Venues.');
     }
 }
 
--- server/models/ticket_venue.js
+++ server/models/ticket_venue.js
@@ -7,6 +7,8 @@ const TicketVenueSchema = new Schema(
         id: { type: Number, required: true, index: { unique: true } },
         location: String,
         url: { type: String, required: true },
+        slug_url: { type: String, required: true },
+        slug: { type: String, required: true },
         country_code: String,
         keywords: { type: String },
         popularity_score: { type: String },
