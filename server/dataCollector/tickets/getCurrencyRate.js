cd0c3060183fd86594e2ded837292002230b17f1
Black Storm <nightyellowstormy@gmail.com>
2022-03-21 09:58:05 +0900

    make ticket type filter

--- server/dataCollector/tickets/getCurrencyRate.js
+++ server/dataCollector/tickets/getCurrencyRate.js
@@ -3,14 +3,13 @@ const Frontend = require('../../models/frontend');
 
 const getCurrencyRate = async (API_KEY) => {
     try {
-        const { data: { rates } } = await axios.get('https://api.exchangeratesapi.io/v1/latest', {
+        const { data: { rates } } = await axios.get('http://api.exchangeratesapi.io/v1/latest', {
             params: {
                 access_key: API_KEY,
-                base: 'USD',
-                symbols: ['CAD', 'EUR', 'GBP', 'AUD', 'JPY'].join(',')
+                base: 'EUR',
+                symbols: ['USD', 'CAD', 'EUR', 'GBP', 'AUD', 'JPY'].join(',')
             }
         });
-        rates.USD = 1;
         await Frontend.findOneAndUpdate({ name: 'currency_rate', value: rates });
         console.log('Got Currency Rate.');
 
