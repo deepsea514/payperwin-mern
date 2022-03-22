346bb24c1d4dc6f9e54fbb6d535e0ad3712c3e6d
Snow <neoprince1992@gmail.com>
2022-03-23 08:57:35 +0900

    change rate api

--- server/dataCollector/tickets/getCurrencyRate.js
+++ server/dataCollector/tickets/getCurrencyRate.js
@@ -3,11 +3,12 @@ const Frontend = require('../../models/frontend');
 
 const getCurrencyRate = async (API_KEY) => {
     try {
-        const { data: { rates } } = await axios.get('http://api.exchangeratesapi.io/v1/latest', {
+        const { data: { rates } } = await axios.get('https://api.getgeoapi.com/v2/currency/convert', {
             params: {
-                access_key: API_KEY,
-                base: 'EUR',
-                symbols: ['USD', 'CAD', 'EUR', 'GBP', 'AUD', 'JPY'].join(',')
+                api_key: API_KEY,//'82ed1714be13a2497b86a9905c77f5bdf4e766b7',
+                from: 'USD',
+                to: 'CAD',
+                format: 'json',
             }
         });
         await Frontend.findOneAndUpdate({ name: 'currency_rate' }, { value: rates });
