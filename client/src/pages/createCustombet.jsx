44dde948f985f675145ea59ee8c4acc28206ce72
Black Storm <nightyellowstormy@gmail.com>
2022-03-30 09:58:13 +0900

    add tooltip for additional high staker

--- client/src/pages/createCustombet.jsx
+++ client/src/pages/createCustombet.jsx
@@ -7,7 +7,7 @@ import CustomDatePicker from '../components/customDatePicker';
 import { createCustomBet } from '../redux/services';
 import { showErrorToast, showSuccessToast } from '../libs/toast';
 import EventSearchModal from '../components/EventSearchModal';
-import { FormControlLabel, Checkbox } from '@material-ui/core';
+import { FormControlLabel, Checkbox, Tooltip } from '@material-ui/core';
 import dateformat from 'dateformat';
 import { Link } from 'react-router-dom';
 
@@ -157,19 +157,23 @@ const EventDetails = ({ touched, errors, values, setFieldTouched, setFieldValue,
                 />
                 {errors.maximumRisk && <div className="form-error">{errors.maximumRisk}</div>}
             </div>
-            <FormControlLabel
-                control={
-                    <Checkbox
-                        checked={values.allowAdditional}
-                        onChange={(e) => setFieldValue('allowAdditional', e.target.checked)}
-                        name="allowAdditional"
-                        color="secondary"
-                    />
-                }
-                margin="normal"
-                labelPlacement="end"
-                label="Allow Additional High Staker"
-            />
+            <Tooltip
+                arrow
+                title={<p className='text-white mb-0 p-1'>Allows other to join your bet to share the risk</p>}>
+                <FormControlLabel
+                    control={
+                        <Checkbox
+                            checked={values.allowAdditional}
+                            onChange={(e) => setFieldValue('allowAdditional', e.target.checked)}
+                            name="allowAdditional"
+                            color="secondary"
+                        />
+                    }
+                    margin="normal"
+                    labelPlacement="end"
+                    label="Allow Additional High Staker"
+                />
+            </Tooltip>
         </>
     )
 }
