f6c26762abd4cafce7894e5fc89de6130751b08d
Snow <neoprince1992@gmail.com>
2022-04-19 16:42:39 +0900

    update widget getting

--- server/widgetRoutes.js
+++ server/widgetRoutes.js
@@ -3,37 +3,19 @@ const widgetRouter = require('express').Router();
 //Models
 const Sport = require('./models/sport');
 
-//local helpers
-// const generatePPWShopSignature = require('./libs/generatePPWShopSignature');
-
-const signatureCheck = async (req, res, next) => {
-    next();
-}
-
 widgetRouter.get('/',
-    signatureCheck,
     async (req, res) => {
-        const filterSports = [
-            'Basketball',
-            'Ice Hockey'
-        ]
+        const { sport, league: league_id } = req.query;
         try {
-            const sportDatas = await Sport.find({ name: { $in: filterSports } });
-            let NBAEvents = [], NHLEvents = [];
-            sportDatas.map(sportData => {
-                if (sportData.name == filterSports[0]) {
-                    const leagues = sportData.leagues.find(league => league.name == 'NBA')
-                    if (leagues)
-                        NBAEvents = leagues.events.filter(event => new Date(event.startDate).getTime() >= new Date().getTime())
-                }
-                if (sportData.name == filterSports[1]) {
-                    const leagues = sportData.leagues.find(league => league.name == 'NHL')
-                    if (leagues)
-                        NHLEvents = leagues.events.filter(event => new Date(event.startDate).getTime() >= new Date().getTime())
-                }
-            });
-            
-            return res.json({ success: true, NBAEvents, NHLEvents });
+            const sportData = await Sport.findOne({ shortName: sport });
+            let events = [];
+            if (sportData) {
+                const leagues = sportData.leagues.find(league => league.originId == league_id)
+                if (leagues)
+                    events = leagues.events.filter(event => new Date(event.startDate).getTime() >= new Date().getTime())
+            }
+
+            return res.json({ success: true, events });
         } catch (error) {
             console.error(error);
             return res.status(500).json({
